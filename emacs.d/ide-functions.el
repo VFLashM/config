b6306ace7d821dd9816da4e1211180e1964a34a3
Valerii Lashmanov <vflashm@gmail.com>
2014-09-22 16:25:29 -0400

    major update of ide-functions

--- emacs.d/ide-functions.el
+++ emacs.d/ide-functions.el
@@ -1,23 +1,27 @@
 (require 'cl-lib)
-(setq project-path (car (car projects)))
-(setq project-name (car (cdr (car projects))))
+
+;;; Code:
 
 (defun choose-variant (list)
-  (popup-menu* list))
+  (if (and list (> (length list) 0))
+      (if (> (length list) 1)
+          (popup-menu* list :isearch t)
+        (car list))))
 
 (defun get-file-project-dir (path)
   (if (or (equal path nil) (equal path ""))
       nil
-    (if (string-suffix-p "/" path)
-        (get-file-project-path (substring path 0 -1))
-      (if (file-exists-p (concat path "/.ropeproject"))
-          (concat path "/")
-        (get-file-project-path (file-name-directory path))
-        ))))
+    (let ((dir-path (file-name-as-directory path)))
+      (if (or
+           (file-exists-p (concat dir-path ".ropeproject"))
+           (file-exists-p (concat dir-path ".git")))
+          dir-path
+        (get-file-project-dir (file-name-directory (substring dir-path 0 -1)))
+        )
+      )))
 
-(defun fix-slashes (fpath)
-  (replace-regexp-in-string "\\\\" "/" fpath)
-  )
+(defun get-current-project-dir ()
+  (get-file-project-dir (buffer-file-name (current-buffer))))
 
 (defun choose-buffer ()
   (interactive)
@@ -27,25 +31,74 @@
                       )
                      ))
 
-(setq file-relative-paths '("./" "Src/" "../"))
-(setq qt-dir
-      (if (file-exists-p "/usr/include/qt4/")
-          "/usr/include/qt4/"
-        (if (getenv "QTDIR")
-            (fix-slashes (getenv "QTDIR"))
-          ""
-          )
-        ))
-(setq ide-setted-up nil)
+(defun choose-project-file (project-dir)
+  (if project-dir
+      (let ((path (choose-variant (fs-list-files-recursively project-dir))))
+        (if path
+            (find-file (concat project-dir path))))))
 
-(defun find-tag-under-cursor ()
-  "finds tag under cursor"
+(defun choose-current-project-file ()
   (interactive)
-  (find-tag
-   (funcall (or find-tag-default-function
-		(get major-mode 'find-tag-default-function)
-		'find-tag-default)))
+  (choose-project-file (get-current-project-dir)))
+
+(defun choose-project ()
+  (interactive)
+  (let ((project (choose-variant (delete-dups (delq nil (mapcar 'get-file-project-dir (mapcar 'buffer-file-name (buffer-list))))))))
+        (if project
+            (choose-project-file project))))
+
+(defun vc-status-project ()
+  (interactive)
+  (let ((pdir (get-current-project-dir)))
+    (if pdir
+        (vc-dir pdir)
+      (call-interactively 'vc-dir))))
+
+(defun find-corresponding-ext-file (find-ext find-paths)
+  "find corresponding header file"
+  (set 'fname (concat 
+           (concat (file-name-directory (buffer-file-name)) (car find-paths)) 
+           (file-name-nondirectory (concat (file-name-sans-extension (buffer-file-name)) "." find-ext))))
+  (if (file-exists-p fname) 
+      (find-file fname)
+    (if (> (length find-paths) 1) 
+    (find-corresponding-ext-file find-ext (cdr find-paths))))
   )
+
+
+(defun get-linked-extensions ()
+  '("h" "cpp" "c" "mm" "m" "hpp" "cxx"))
+
+(defun get-linked-paths ()
+  '("" "../" "Src/"))
+
+(defun get-linked-files (path)
+  (setq path (file-truename path))
+  (cl-remove-if-not
+   'file-exists-p
+   (mapcar
+    (lambda (x) (file-truename x))
+    (aux-flatten
+     (mapcar
+      (lambda (vpath)
+        (mapcar
+         (lambda (ext) (concat (file-name-sans-extension vpath) "." ext))
+         (get-linked-extensions)))
+      (mapcar
+       (lambda (relpath)
+         (concat (file-name-directory path) relpath (file-name-nondirectory path)))
+       (get-linked-paths)))))))
+
+(defun cycle-linked-file ()
+  "find corresponding header file"
+  (interactive)
+  (if (member (file-name-extension (buffer-file-name)) (get-linked-extensions))
+      (find-file
+       (aux-next-cycle-element
+        (get-linked-files (buffer-file-name))
+        (file-truename (buffer-file-name))))))
+
+;;; build system
 
 (defun save-and-run ()
   "save, make and run"
@@ -79,88 +132,7 @@
                    ".cpp")))
   (message "compiled!"))
 
-(defun find-corresponding-ext-file (find-ext find-paths)
-  "find corresponding header file"
-  (set 'fname (concat 
-           (concat (file-name-directory (buffer-file-name)) (car find-paths)) 
-           (file-name-nondirectory (concat (file-name-sans-extension (buffer-file-name)) "." find-ext))))
-  (if (file-exists-p fname) 
-      (find-file fname)
-    (if (> (length find-paths) 1) 
-    (find-corresponding-ext-file find-ext (cdr find-paths))))
-  )
-
-(defun find-corresponding-file ()
-  "find corresponding header file"
-  (interactive)
-  (set 'ext (file-name-extension (buffer-file-name)))
-  (if (equal ext "h")
-      (find-corresponding-ext-file "cpp" file-relative-paths)
-    (if (equal ext "cpp")
-    (find-corresponding-ext-file "h" file-relative-paths)))
-  )
-
-(defun ebrowse-project ()
-  "browse project classes"
-  (interactive)
-  (if (get-buffer "*Tree*") (kill-buffer (get-buffer "*Tree*")))
-  (shell-command
-   (concat "cd " project-path " && find * -iname '*.cpp' -or -iname '*.h' | ebrowse"))
-  (find-file-read-only 
-   (concat project-path "/BROWSE"))
-  )
-
-(defun change-project ()
-  (interactive)
-  (check-setup-ide)
-
-  (shell-command 
-   (concat "echo " (mapconcat 'identity (mapcar (lambda (x) (concat (car (cdr x)) " " (car x) "?" (car (cdr x)) " " (car x))) projects) " ") " | qmenu -i 3" )
-   )
-
-  (set-buffer (get-buffer "*Shell Command Output*"))
-  (if (> (length (buffer-string)) 0)
-      (let (
-	    (project-pair (split-string (buffer-string) "?"))
-	    )
-	(setq project-path (car project-pair))
-	(setq project-name (car (cdr project-pair)))
-	)
-    )
-  )
-
-(defun find-file-in-project () 
-  (interactive)
-  (check-setup-ide)
-  (shell-command
-   (concat "chooseFile " project-path " " project-path ".qmenu.cache " project-name))
-  (set-buffer (get-buffer "*Shell Command Output*"))
-  (if (> (length (buffer-string)) 0)
-      (find-file (buffer-string)))
-  )
-
-(defun svnbrowse-project ()
-  "browse svn tree of project"
-  (interactive)
-  (svn-status project-path))
-
-(defun etag-project ()
-  "browse project classes"
-  (interactive)
-  (shell-command
-   (concat "etagProject " project-path))
-  (setq tags-file-name (concat project-path "/TAGS"))
-  )
-
-(defun check-setup-ide ()
-  "setup ide if not setted up"
-  (interactive)
-  (if 
-      (not ide-setted-up)
-      (setup-ide)
-    )
-  (setq ide-setted-up t)
-  )
+;;; Entry point
 
 (defun setup-ide ()
   (setq-default flycheck-disabled-checkers '(python-flake8))
@@ -181,74 +153,4 @@
               (define-key yas-minor-mode-map (kbd "TAB") nil)
               (define-key yas-minor-mode-map (kbd "C-`") 'yas-expand)
               (local-set-key (kbd "<C-return>") 'rope-goto-definition)
-              ))
-)
-
-(defun cut-trailing-slash (fpath)
-  (if (or
-       (equal (substring fpath -1 (length fpath)) "/")
-       (equal (substring fpath -1 (length fpath)) "\\"))
-      (substring fpath 0 -1)
-    fpath
-    )
-  )
-
-(defun cut-extension (fpath)
-  (if (equal (substring fpath -1 (length fpath)) ".")
-      (substring fpath 0 -1)
-    (cut-extension (substring fpath 0 -1))
-    )
-  )
-
-(defun is-capital-char (string position)
-  (if (< position 0)
-      (is-capital-char string (+ position (length string)))
-    (equal (upcase (substring string position (+ position 1))) (substring string position (+ position 1)))
-    )
-  )
-
-(defun format-string-as-preprocessor-header-part-loop (pref suf)
-  (if 
-      (and (> (length pref) 0) (> (length suf) 0))
-      (if (and (not (is-capital-char pref -1)) (is-capital-char suf 0))
-      (concat "_" (upcase (substring suf 0 1)))
-    (upcase (substring suf 0 1))
-    
-    )
-    "")
-  )
-
-(defun format-string-as-preprocessor-header-part (string)
-  (let (result)
-    (dotimes (number (length string) result)
-      (setq result (concat result 
-               (format-string-as-preprocessor-header-part-loop
-                (substring string 0 number) 
-                (substring string number (length string)))
-               )
-        )
-      )
-    result)
-  )
-
-(defun format-string-as-preprocessor-header (string)
-  (concat "__" (upcase (substring string 0 1)) (format-string-as-preprocessor-header-part (cut-extension string)) "__")
-  )
-
-(defun current-file-preprocessor-header ()
-  (format-string-as-preprocessor-header (file-name-nondirectory (buffer-file-name)))
-  )
-
-(defun decorate-h-file ()
-  (interactive)
-  (save-excursion
-    (goto-char (point-min))
-    (insert 
-     (concat "#ifndef " (current-file-preprocessor-header) "\n#define " (current-file-preprocessor-header) "\n\n")
-     )
-    (goto-char (point-max))
-    (insert 
-     (concat "\n#endif // " (current-file-preprocessor-header) "\n")
-     )
-    )
-  )
+              )))
