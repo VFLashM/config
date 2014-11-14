0e4b74510d4d096ba318118afea604bc095e4678
Valerii Lashmanov <vflashm@gmail.com>
2014-11-14 11:52:06 -0500

    missing flatten added

--- emacs.d/ide-functions.el
+++ emacs.d/ide-functions.el
@@ -18,7 +18,7 @@
     (if (file-exists-p path)
         (with-temp-buffer 
           (insert-file-contents path)
-          (mapcar 'file-name-as-directory (mapcar 'expand-file-name (mapcar 'f-glob (split-string (buffer-string) "\n" t)))))
+          (mapcar 'file-name-as-directory (mapcar 'expand-file-name (aux-flatten (mapcar 'f-glob (split-string (buffer-string) "\n" t))))))
       (write-region "" nil path))))
 
 (defun find-matching-permanent-project-for-path (path projects)
